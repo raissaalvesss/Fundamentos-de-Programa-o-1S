5f1c718d2fd67fa84c5f78e977e51e816b7e2e1b
RaissaAlves <raissa.571011@gmail.com>
2026-02-26 21:40:28 -0300

    Add files via upload

---
+++ raiz RA.py
@@ -0,0 +1,12 @@
+print ("###################################################################################")
+print ("                                RAIZ QUADRADA                                      ")
+print ("###################################################################################")
+
+import math
+a = input("Entre com o primeiro numero e tecle enter")
+
+a = int(a)
+
+print(math.sqrt(a))
+
+input() #Apenas para esperar o enter de fechar a janela
---
+++ subtrator RA.py
@@ -0,0 +1,13 @@
+print ("###################################################################################")
+print ("                                SUBTRATOR                                          ")
+print ("###################################################################################")
+
+a = input("Entre com o primeiro numero e tecle enter")
+b = input("Entre com o segundo numero e tecle enter")
+
+a = int(a)
+b = int(b)
+
+print(a - b)
+
+input() #Apenas para esperar o enter de fechar a janela
